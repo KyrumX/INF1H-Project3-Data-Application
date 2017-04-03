b2030e2510270a666f0230e98d4784027d9e3692
KyrumX <kyrumx@hotmail.nl>
2017-04-03 10:27:55 +0200

    Updated the .gitignore
    Updated the .gitignore to follow the suggestions made by the developers
    of intellij Idea:
    https://intellij-support.jetbrains.com/hc/en-us/articles/206544839-How-to-manage-projects-under-Version-Control-Systems

--- Code/src/Main/Main.java
+++ Code/src/Main/Main.java
@@ -3,7 +3,6 @@ package Main;
 /**
  * Created by Aaron on 2-4-2017.
  * This file will act as the Main file, we will call all code from this file.
- * Please do not commit directly to master, use pull requests
  */
 public class Main {
     public static double versionNumber = 1.0;
