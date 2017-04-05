a80014fead384f2f3c0a9ab54a709ff393ea92af
KyrumX <kyrumx@hotmail.nl>
2017-04-05 20:17:11 +0200

    Added postgresql library

---
+++ Application Code/src/Database/ConnectDatabase.java
@@ -0,0 +1,12 @@
+package Database;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by aaron on 5-4-2017.
+ */
+public class ConnectDatabase {
+
+}
