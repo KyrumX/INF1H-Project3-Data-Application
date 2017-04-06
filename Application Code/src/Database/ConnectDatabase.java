7354dd1973c8e55add37ed43fb6b64824faed5fe
KyrumX <kyrumx@hotmail.nl>
2017-04-06 09:14:50 +0200

    Added support for SQL Databases from Postgresql; Updated the name of TrialGraph

--- Application Code/src/Database/ConnectDatabase.java
+++ Application Code/src/Database/ConnectDatabase.java
@@ -8,5 +8,24 @@ import java.sql.SQLException;
  * Created by aaron on 5-4-2017.
  */
 public class ConnectDatabase {
+    private final String url = "jdbc:postgresql://localhost/project2";
+    private final String user = "postgres";
+    private final String password = "kaas123";
 
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        ConnectDatabase trial = new ConnectDatabase();
+        trial.connect();
+    }
 }
