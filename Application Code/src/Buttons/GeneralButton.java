aefd2d5c9f3b64bf12125601a78428ac26ac9ddc
Vitalynx <selimaydi@gmail.com>
2017-04-06 03:00:37 +0200

    General button class

---
+++ Application Code/src/Buttons/GeneralButton.java
@@ -0,0 +1,29 @@
+package Buttons;
+import Effects.Shadoweffect;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+/**
+ * Created by Selim on 6/4/2017.
+ */
+public class GeneralButton extends AbstractButtonClass {
+    private Button button;
+    public GeneralButton(int OffsetX,int OffsetY, String label, EventHandler<ActionEvent> x, boolean isExitButton){
+        this.button = new Button();
+        this.button.setText(label);
+        this.button.setOnAction(x);
+        this.button.setEffect(new Shadoweffect(0.5).getShadow());
+        this.button.setTranslateX(OffsetX);
+        this.button.setTranslateY(OffsetY);
+
+        if (isExitButton) {
+            this.button.getStyleClass().add("exitButton");
+        }
+
+    }
+    public Button getButton(){
+        return this.button;
+    }
+
+}
