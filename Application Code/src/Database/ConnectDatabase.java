b1b0b8d2db83c7817b1df02e0dfa90047e63d40c
KyrumX <kyrumx@hotmail.nl>
2017-04-10 16:11:19 +0200

    Made the getGarages() method more dynamic.

--- Application Code/src/Database/ConnectDatabase.java
+++ Application Code/src/Database/ConnectDatabase.java
@@ -26,53 +26,24 @@ public class ConnectDatabase {
     }
 
     public HashMap getGarages() {
-        double deelCentrum = 0;
-        double deelPrins = 0;
-        double deelIJssel = 0;
-        double deelFeij = 0;
-        double deelChar = 0;
-        double deelOver = 0;
         HashMap<String, Double> newHashMap = new HashMap<String, Double>();
         try {
             Statement stmt = conn.createStatement();
             ResultSet rs;
 
-            rs = stmt.executeQuery("SELECT garagenaam, deelgemeente FROM Garages");
+            rs = stmt.executeQuery("SELECT deelgemeente, COUNT(garagenaam) FROM garages GROUP BY deelgemeente");
             while ( rs.next() ) {
-                String garageNaam = rs.getString("garagenaam");
-                String deelGemeente = rs.getString("deelgemeente");
-//                newHashMap.put("Turkey Burger", 13);
-                System.out.println(deelGemeente);
-                if(deelGemeente.equals("prins alexander")) {
-                    deelPrins += 1;
-                }
-                else if(deelGemeente.equals("stadscentrum")) {
-                    deelCentrum += 1;
-                }
-                else if(deelGemeente.equals("charlois")) {
-                    deelChar += 1;
-                }
-                else if(deelGemeente.equals("ijsselmonde")) {
-                    deelIJssel += 1;
-                }
-                else if(deelGemeente.equals("overschie")) {
-                    deelOver += 1;
-                }
-                else if(deelGemeente.equals("feijenoord")) {
-                    deelFeij += 1;
-                }
+                String deelGemeenteNaam = rs.getString("deelgemeente");
+                double garageNaamCount = rs.getDouble("COUNT");
+                System.out.print(garageNaamCount + " ");
+                System.out.println(deelGemeenteNaam);
+                newHashMap.put(deelGemeenteNaam, garageNaamCount);
             }
             conn.close();
         } catch (Exception e) {
             System.err.println("Got an exception!");
             System.err.println(e.getMessage());
         }
-        newHashMap.put("Stadscentrum", deelCentrum);
-        newHashMap.put("Charlois", deelChar);
-        newHashMap.put("Prins Alexnader", deelPrins);
-        newHashMap.put("IJsselmonde", deelIJssel);
-        newHashMap.put("Overschie", deelOver);
-        newHashMap.put("Feijenoord", deelFeij);
         return newHashMap;
     }
 }
