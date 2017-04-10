594b50e009f7850f4665e21ea1ab4f8f51471961
KyrumX <kyrumx@hotmail.nl>
2017-04-10 10:41:39 +0200

    Added a new BarGraph

---
+++ Application Code/src/Graphs/BarGraph.java
@@ -0,0 +1,42 @@
+package Graphs;
+
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import java.util.HashMap;
+
+
+/**
+ * Created by aaron on 10-4-2017.
+ */
+public class BarGraph {
+    public BarChart chart;
+
+    public BarGraph(HashMap<String, Double> chartData, String deelGemeente) {
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> barGraph =
+                new BarChart<String,Number>(xAxis,yAxis);
+        barGraph.setTitle("Percentage autodiefstal");
+        xAxis.setLabel("Deelgemeente: " + deelGemeente);
+        yAxis.setLabel("Procent");
+
+        for (String item : chartData.keySet()) {
+            XYChart.Series series = new XYChart.Series();
+            series.setName(item);
+            series.getData().add(new XYChart.Data("", chartData.get(item)));
+            barGraph.getData().addAll(series);
+        }
+        chart = barGraph;
+
+
+    }
+
+    public BarChart getGraph() {
+        return chart;
+    }
+
+
+}
