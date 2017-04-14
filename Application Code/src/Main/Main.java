2ab2ad0720557939611dce8c1ff684dc200a82de
Vitalynx <selimaydi@gmail.com>
2017-04-14 09:05:07 +0200

    hover change

--- Application Code/src/Main/Main.java
+++ Application Code/src/Main/Main.java
@@ -63,7 +63,7 @@ public class Main extends Application {
 
         public WindowButtons() {
             Button closeBtn = new Button("X");
-
+            closeBtn.setId("closebutton");
             closeBtn.setOnAction(new EventHandler<ActionEvent>() {
 
                 @Override
