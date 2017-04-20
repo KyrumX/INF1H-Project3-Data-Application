8aa9be29c26f9198cb75255a9fd485f84efcde11
Aaron <kyrumx@hotmail.nl>
2017-04-20 19:20:19 +0200

    Fixing translate error 1

--- Application Code/src/Graphs/BarGraph.java
+++ Application Code/src/Graphs/BarGraph.java
@@ -29,7 +29,7 @@ public class BarGraph {
         final NumberAxis yAxis = new NumberAxis();
         final BarChart<String,Number> barGraph =
                 new BarChart<String,Number>(xAxis,yAxis);
-        barGraph.setTitle("Percentage of car thefts victems");
+        barGraph.setTitle("Percentage of car thefts victims");
         xAxis.setLabel("City district");
         yAxis.setLabel("Percentage");
 
