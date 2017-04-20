e7b05efdae01a5d94912985671767c6a409a4325
Ryan0o0 <ryanwilson219@gmail.com>
2017-04-20 12:55:28 +0200

    CSV Parser 2
    Made the CSV Parser to revert the csv files in data

--- Application Code/src/Events/CSVReader.java
+++ Application Code/src/Events/CSVReader.java
@@ -1,33 +1,41 @@
 package Events;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import Database.ConnectDatabase;
+import java.io.*;
 
 /**
  * Created by Aaron on 16-4-2017.
  */
+
 public class CSVReader {
     public static void main(String[] args) {
 
-        String csvFile = "C:/Users/Selim/Desktop/umlstuff/parking.csv";
+        String csvFile = "C:/Users/Ryan Wilson/Downloads/parking.csv";        //Verander de naam van de User naar je eigen naam
         BufferedReader br = null;
         String line = "";
+        String a = null;
+        int b =  0;
+        int c =  0;
 
         //Soort seperator gebruikt in CSV file, in dit geval een ';'
         String cvsSplitBy = ";";
 
         try {
-
+            ConnectDatabase DataBase = new ConnectDatabase();
+            DataBase.connect();
             br = new BufferedReader(new FileReader(csvFile));
             br.readLine();
             while ((line = br.readLine()) != null) {
+
                 String[] lineSplitter = line.split(cvsSplitBy);
 
                 //Pak het vierde en vijfde item van elke regel, de naam van de garage en de code
-                System.out.println("Naam van de garage: " + lineSplitter[3] + ", de code van de garage= " + lineSplitter[4]);
+                System.out.println("Naam van de garage: " + lineSplitter[3] + ", Longditude: " + lineSplitter[2] + ", Latitude: " + lineSplitter[1]);
+                a = lineSplitter[3];
+                float b2 = Float.parseFloat(lineSplitter[2]);
+                float c2 = Float.parseFloat(lineSplitter[1]);
 
+                DataBase.parser(a, b2, c2);
             }
 
         } catch (FileNotFoundException e) {
